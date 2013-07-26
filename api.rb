146a46f987f850faf1889c9ae87be0ea2c3319f5
Eadon <eadonj@gmail.com>
2013-07-26 00:06:05 -0700

    cleaning up code

--- api.rb
+++ api.rb
@@ -6,19 +6,19 @@ require 'json'
 post '/' do
   content = JSON.parse(request.env["rack.request.form_vars"])
   recipient = content["to"].match(/.*<(.*)>/)[1]
-  Pony.mail(to: recipient, 
-  	:from => "noreply@example.com", 
-  	subject: content['subject'], 
-  	body: content['body'],
-  	:via => :smtp,
-	  :via_options => {
-	    :address => 'smtp.sendgrid.net',
-	    :port => '587',
-	    :domain => 'heroku.com',
-	    :user_name => ENV['SENDGRID_USERNAME'],
-	    :password => ENV['SENDGRID_PASSWORD'],
-	    :authentication => :plain,
-	    :enable_starttls_auto => true
-	  }
-	)
+  Pony.mail to: recipient, 
+				  	from: "noreply@example.com", 
+				  	subject: content['subject'], 
+				  	body: content['body'],
+				  	via: :smtp,
+					  via_options: {
+					    address: 'smtp.sendgrid.net',
+					    port: '587',
+					    domain: 'heroku.com',
+					    user_name: ENV['SENDGRID_USERNAME'],
+					    password: ENV['SENDGRID_PASSWORD'],
+					    authentication: :plain,
+					    enable_starttls_auto: true
+					  }
+	
 end
