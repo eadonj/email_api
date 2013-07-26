628f1a5c4f77a83ede5597e87e9caff432aa0388
Eadon <eadonj@gmail.com>
2013-07-26 11:29:18 -0700

    cleaned up spacing

--- api.rb
+++ api.rb
@@ -3,26 +3,26 @@ require 'pony'
 require 'json'
 
 post '/' do
-	content_type :json
-	content = JSON.parse(request.body.read) 
+  content_type :json
+  content = JSON.parse(request.body.read) 
 
   Pony.mail( 
     to: content["to"], 
-	  from: "noreply@example.com", 
-  	subject: content['subject'], 
-  	body: content['body'],
-  	via: :smtp,
-	  via_options: {
-	    address: 'smtp.sendgrid.net',
-	    port: '587',
-	    domain: 'heroku.com',
-	    user_name: ENV['SENDGRID_USERNAME'],
-	    password: ENV['SENDGRID_PASSWORD'],
-	    authentication: :plain,
-	    enable_starttls_auto: true
-	  }
-	)
+    from: "noreply@example.com", 
+    subject: content['subject'], 
+    body: content['body'],
+    via: :smtp,
+    via_options: {
+      address: 'smtp.sendgrid.net',
+      port: '587',
+      domain: 'heroku.com',
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+  )
 
   status 200
-	body ""
+  body ""
 end
