1b0eb8ddc9e8519a9fb61e70d4b4888c101c4846
Eadon <eadonj@gmail.com>
2013-07-25 22:23:48 -0700

    added pony for email

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'http://rubygems.org'
 
 gem 'sinatra'
-gem 'shotgun'
 gem 'json'
+gem 'shotgun'
+gem 'pony'
--- api.rb
+++ api.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
+require 'pony'
 require 'json'
 
 post '/' do
+  content_type :json
+  Pony.mail(:to => 'eadonj@gmail.com', :from => 'noreply@example.com', :subject => "subject", :body => "body")
 end
+
